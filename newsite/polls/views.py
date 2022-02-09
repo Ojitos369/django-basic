c60248180a4a5c80d4cb5460a4bf2b7494d7bf62
ojitos369 <ojitos369@gmail.com>
2022-02-09 14:51:43 -0600

    form_backend

--- newsite/polls/views.py
+++ newsite/polls/views.py
@@ -2,7 +2,8 @@
 
 # Django
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # User
 from .models import *
@@ -25,8 +26,25 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta {question_id}")
+    question = get_object_or_404(Question, pk = question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        print(request.POST)
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': "No seleccionaste ninguna opción"
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
